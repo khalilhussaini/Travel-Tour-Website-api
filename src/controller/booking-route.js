b655943462e5779505cc3c33e2428d08b1f58f87
Mehmet Dogan <mehmet.dogan@hicoders.ch>
2023-06-24 16:29:40 +0200

    delete unnecessary files for bookings

--- src/controller/booking-route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import createBooking from '../controller/booking-controller.js';
-
-const router = express.Router();
-
-// Route to handle booking submission
-router.post('/', createBooking);
-
-export default router;
